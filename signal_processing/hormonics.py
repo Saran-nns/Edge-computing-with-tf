287624406861e7dca64b5064c24c89bb8764e989
Saran-nns <saran_nns@hotmail.com>
2020-10-14 22:49:14 +0200

    max freq components, harmonics, iir filters

--- signal_processing/hormonics.py
+++ signal_processing/hormonics.py
@@ -132,8 +132,8 @@ def get_max_harmonics(sig: array,fs: float,maxharms:int):
     Search for a maximum value of a range of possible fundamental frequencies
     Args:
         sig (array): Signal
-        fs (float): [description]
-        maxharms (int): [description]
+        fs (float): Sampling rate of the signal
+        maxharms (int): Max harmonics
     """  
     c, pitch = hps(sig,fs,maxharms)
     for x in range(2, maxharms):
@@ -145,3 +145,75 @@ def get_max_harmonics(sig: array,fs: float,maxharms:int):
         plt.plot(maxharms, x)
         plt.plot(np.log(c))
     show()
+
+def freq_comp(signal:array,sample_rate:int):
+    """Frequency components of the signal
+
+    Args:
+        signal (array): Signal
+        sample_rate (int): Sample rate of the signal
+
+    Returns:
+        array: Frequencies
+    """
+    
+    # Define the sample spacing and window size.
+    dT = 1.0/sample_rate
+    T_window = 50e-3   # 50ms ; window time frame
+    N_window = int(T_window * sample_rate)  # 440  
+    N_data = len(signal)  
+
+    # 1. Get the window profile
+    window = get_window('hamming', N_window)   # Multiply the segments of data using hamming window func
+
+    # 2. Set up the FFT
+    result = []
+    start = 0
+    while (start < N_data - N_window):
+        end = start + N_window
+        result.append(fftshift(fft(window*signal[start:end])))
+        start = end
+
+    result.append(fftshift(fft(window*signal[-N_window:])))
+    result = np.array(result,result[0].dtype)
+    
+    return result
+
+def plot_max_harmonics(sig: array,f0: float, fs: float,order, maxharms:int):
+    """
+    Estimate peak frequency using harmonic product spectrum (HPS)
+    Harmonic product spectrum: Measures the maximum coincidence for harmonics for each spectral frame
+    Search for a maximum value of a range of possible fundamental frequencies
+    Args:
+        sig (array): Signal
+        f0 (float): Fundamental freq
+        fs (float): Sampling rate
+        maxharms (int): Max harmonics
+    """  
+    z = HarmonicPowerSpectrum(sig, f0, fs, order = 1,maxharms=0)
+    harm_pow_spec = z.hps
+    plt.figure()
+    plt.plot(harm_pow_spec)
+    plt.title("Max Harmonics for the range same as fundamental frequencies Bp filtered in Order 0 and max harmonic psectum 0")
+    return plt.show()
+
+def plot_frequency_components(sig: array,f0: float, fs: float,order, maxharms:int):
+    """
+    Estimate peak frequency using harmonic product spectrum (HPS)
+    Harmonic product spectrum: Measures the maximum coincidence for harmonics for each spectral frame
+    Search for a maximum value of a range of possible fundamental frequencies
+    Args:
+        sig (array): Signal
+        f0 (float): Fundamental freq
+        fs (float): Sampling rate
+        maxharms (int): Max harmonics
+    """
+    z = HarmonicPowerSpectrum(sig, f0, fs, order = 1,maxharms=0)
+    harm_pow_spec = z.hps
+    
+    freq_comp_hps = freq_comp(harm_pow_spec,fs)
+    plt.figure()
+    plt.plot(freq_comp_hps)
+    plt.title("""Frequency components(in logarithmix scale) of harmonic spectrum of filtered training data. 
+                A harmonic set of two pitches contributing significantly to this piano chord""")
+    return plt.show()
